08cc9b8a211134073c84d7282e40842b051fe4b9
Fabian Ponce <me@fabianponce.com>
2023-10-03 12:28:01 -0400

    fix: subnet tags is None causes error

--- aws_subnet_ip_utilization.py
+++ aws_subnet_ip_utilization.py
@@ -19,7 +19,10 @@ def tag_value(s: str) -> str:
     return s
 
 def subnet_aws_tags(subnet):
-        return [ f"{tag_value(d['Key'])}:{tag_value(d['Value'])}" for d in subnet.tags ]
+        if subnet.tags is None:
+            return []
+        else:
+            return [ f"{tag_value(d['Key'])}:{tag_value(d['Value'])}" for d in subnet.tags ]
 
 class AWSSubnetIPUtilizationCheck(AgentCheck):
     def __init__(self, name, init_config, agentConfig, instances=None):
@@ -34,7 +37,7 @@ class AWSSubnetIPUtilizationCheck(AgentCheck):
             remaining = subnet.available_ip_address_count
             total = subnet_size(subnet.cidr_block)
 
-            tags = [f"subnet:{subnet.id}"] + subnet_aws_tags(subnet), self.instance.get('tags', [])
+            tags = [f"subnet:{subnet.id}"] + subnet_aws_tags(subnet) + self.instance.get('tags', [])
 
             self.gauge('aws.ec2.subnet.available_addresses', remaining, tags=tags)
             self.gauge('aws.ec2.subnet.total_addresses', total, tags=tags)
